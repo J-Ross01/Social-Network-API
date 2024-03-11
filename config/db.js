f3e7f1515285cd15f5d1243eb4558f49c0b31898
Jarrett Delaney <jarrettdelaney1@gmail.com>
2024-03-10 20:31:34 -0700

    seeded db.json

---
+++ config/db.js
@@ -0,0 +1,29 @@
+{
+    "users"; [
+      {
+        "username": "user1",
+        "email": "user1@example.com",
+        "thoughts": [],
+        "friends": []
+      },
+      {
+        "username": "user2",
+        "email": "user2@example.com",
+        "thoughts": [],
+        "friends": []
+      }
+    ],
+    "thoughts"; [
+      {
+        "thoughtText": "Here's a cool thought...",
+        "username": "user1",
+        "reactions": []
+      },
+      {
+        "thoughtText": "Another thought here...",
+        "username": "user2",
+        "reactions": []
+      }
+    ]
+  }
+  
